fbe3a1fdef773df9179f87296ee297c2cecbd3d7
Jayant <vaynoob@outlook.com>
2025-01-27 18:21:15 +0800

    init repository

---
+++ src/main/java/org/jayanat/dashboard/LogDashboardApplication.java
@@ -0,0 +1,13 @@
+package org.jayanat.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogDashboardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/jayanat/dashboard/LogDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jayanat.dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
